bc5955990c92f9131b1a941d91614f1ea8032f1d
Jannis R <mail@jannisr.de>
2017-06-20 17:54:28 +0200

    implementation :clap:

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 'use strict'
 
-// todo
+const {format} = require('tip-integrations/tip-www')
+const h = require('hyperscript')
+
+const css = ''
+
+const store = {
+	write: (active, amount) => {
+		return Promise.resolve()
+	},
+	read: () => {
+		return Promise.resolve({})
+	}
+}
+
+const load = () => true
+
+const marker = () => document.querySelector('#tip-me-marker')
+
+const photo = '#'
+
+const init = (active = false, tip = 0) => {
+	const update = (active, tip) => {}
+
+	update(active, tip) // init ui
+	return update
+}
+
+module.exports = Object.assign(init, {
+	store, parse, format, css, load, marker, photo
+})
